595937285368353facafeb4439011450bd1b37e0
PedroGuilhermeFariaDuarte <pedro_racers@hotmail.com>
2020-06-06 00:05:17 -0300

    App deploy

---
+++ precache-manifest.d1339311daec3967543da3a2d63c37eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc9a66d0824a2d0adebdda542df2b00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6ac78d7c651b2c995e",
+    "url": "/static/js/2.cc9088f6.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/static/js/2.cc9088f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6542292fcf155c24048",
+    "url": "/static/js/main.9f1f6402.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
